28919c151fc19ed646a8ff242c6921381c71abd9
afsmaira <andre.smaira@alumni.usp.br>
2020-07-03 02:58:47 -0300

    Testes template AWS

--- .github/workflows/send.sh
+++ .github/workflows/send.sh
@@ -1,7 +1,7 @@
 if [[ $CLIENT == "google" ]]; then
-  python3 .gitlabci/send_google.py "$@"
+  python3 .github/workflows/send_google.py "$@"
 elif [[ $CLIENT == "aws" ]]; then
-  python3 .gitlabci/send_aws.py "$@"
+  python3 .github/workflows/send_aws.py "$@"
 else
   echo "Plataforma desconhecida: $CLIENT"
 fi
