83445124fdb66c0fbc7d41669c21dd8607177679
afsmaira <andre.smaira@alumni.usp.br>
2020-07-03 02:51:51 -0300

    Testes template AWS

--- .github/workflows/send.sh
+++ .github/workflows/send.sh
@@ -1,14 +1,7 @@
-if [[ $# -lt 1 ]]; then
-  echo "Usage: send.sh <plataforma> <arquivo1> <arquivo2>..."
-  exit
-fi
-
-plat=$1
-shift
-if [[ $plat == "google" ]]; then
+if [[ $CLIENT == "google" ]]; then
   python3 .gitlabci/send_google.py "$@"
-elif [[ $plat == "aws" ]]; then
+elif [[ $CLIENT == "aws" ]]; then
   python3 .gitlabci/send_aws.py "$@"
 else
-  echo "Plataforma desconhecida: $plat"
+  echo "Plataforma desconhecida: $CLIENT"
 fi
