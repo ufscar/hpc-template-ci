1d3787939a51f99751d6b50a8a0965437d9c08e2
afsmaira <andre.smaira@alumni.usp.br>
2020-07-08 21:02:30 -0300

    Unificação de envio para nuvens

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,6 +1,6 @@
 sudo apt install curl -y
 sudo apt install unzip -y
-curl https://rclone.org/install.sh | bash
+curl https://rclone.org/install.sh | sudo bash
 
 sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list && \
     sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9 && \
