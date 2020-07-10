58edb8d2b0c1fd3b96d617c3e61840d113664c80
afsmaira <andre.smaira@alumni.usp.br>
2020-07-10 16:20:01 -0300

    Novo teste com OMPI

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,12 +1,3 @@
-#sudo apt install curl -y
-#sudo apt install unzip -y
-#curl https://rclone.org/install.sh | sudo bash
-
-#sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list && \
-#    sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9 && \
-#    sudo apt-get update && \
-#    sudo apt-get install -y singularity-container
-
 apk add curl
 apk add unzip
 curl https://rclone.org/install.sh | bash
