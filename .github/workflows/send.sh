6e30c12bdf343fed29a06b355f66c86545656d75
afsmaira <andre.smaira@alumni.usp.br>
2020-07-10 16:26:46 -0300

    Novo teste com OMPI

--- .github/workflows/send.sh
+++ .github/workflows/send.sh
@@ -1,6 +1,5 @@
 echo "Criando imagem singularity..."
-singularity --version
-sudo singularity build -F Singularity.simg "${RECIPE}"
+singularity build -F Singularity.simg "${RECIPE}"
 cp "${RECIPE}" Singularity
 
 echo "Configurando ambiente..."
